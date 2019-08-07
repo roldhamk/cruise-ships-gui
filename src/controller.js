165ef32a9b6c0549770a561d57682bcca7a45951
Rachael Oldham-Knott <roldhamk@gmail.com>
2019-08-07 21:26:31 +0100

    completed sailing challenge

--- src/controller.js
+++ src/controller.js
@@ -1,6 +1,10 @@
 (function exportController() {
-function Controller() {
-    this.initialiseSea;
+function Controller(ship) {
+    this.ship = ship
+    this.initialiseSea();
+    document.querySelector('#sailbutton').addEventListener('click', () => {
+        this.setSail();
+    });
 }
 
 Controller.prototype = {
@@ -30,6 +34,35 @@ Controller.prototype = {
           portsElement.style.width = `${portsElementWidth + 256}px`;
         })
       },
+      renderShip () {
+          const ship = this.ship
+          const shipPortIndex = ship.itinerary.ports.indexOf(ship.currentPort);
+          const portElement = document.querySelector(`[data-port-index='${shipPortIndex}']`);
+          const shipElement = document.querySelector('#ship');
+          shipElement.style.top = `${portElement.offsetTop + 32}px`;
+          shipElement.style.left = `${portElement.offsetLeft - 32}px`;
+        },
+      setSail() {
+        const ship = this.ship
+        const currentPortIndex = ship.itinerary.ports.indexOf(ship.currentPort);
+        const nextPortIndex = currentPortIndex + 1;
+        const nextPortElement = document.querySelector(`[data-port-index='${nextPortIndex}']`)
+        if (!nextPortElement) {
+
+            return alert('End of the line!');
+          
+        }
+        const shipElement = document.querySelector('#ship');
+        const sailInterval = setInterval(() => {
+            const shipLeft = parseInt(shipElement.style.left, 10);
+            if (shipLeft === (nextPortElement.offsetLeft - 32)) {
+                ship.dock();
+                clearInterval(sailInterval);
+            }
+            shipElement.syle.left = `${shipLeft + 1}px`;
+        }, 30);
+        
+        },
     };
 
     if (typeof module !== 'undefined' && module.exports) {
@@ -40,3 +73,9 @@ Controller.prototype = {
     }
   
   }());
+
+  
+
+//if (!nextPortElement) {
+    //return alert('End of the line!');
+    //}
